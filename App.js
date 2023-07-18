6ba8a01deb5c2ff2e7238aa891ccaaf3f5015644
Łukasz Gola <105663541+lukasgola@users.noreply.github.com>
2023-07-18 18:07:31 +0200

    .

--- App.js
+++ App.js
@@ -30,7 +30,6 @@ export default function App() {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
       setIsUser(2);
       // ...
     } else {
