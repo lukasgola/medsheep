777f3d5337339368d0eaa38fa579d5acc01decc8
Łukasz Gola <lukaszgolaofficial@gmail.com>
2024-06-25 13:34:44 +0200

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,126 +1,283 @@
 export const DATA = [
-    {
+  {
       id: 1,
       name: 'Apap Noc',
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      desc: "Apap Noc to lek przeciwbólowy i przeciwgorączkowy, który pomaga złagodzić bóle głowy, gardła, mięśni oraz gorączkę, a także bóle menstruacyjne.",
       availability: true,
       price: 12.99,
       img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/apap_noc.jpg?alt=media&token=ef8bd1a5-8003-4783-b0e9-dcaf27a6b6ba',
       amount: 30,
       symptoms: [
-        {
-          id: 1,
-          name: 'Ból głowy'
-        },
-        {
-          id: 2,
-          name: 'Ból gardła'
-        },
-        {
-          id: 3,
-          name: 'Gorączka'
-        },
-        {
-          id: 4,
-          name: 'Ból mięśni'
-        },
-        {
-          id: 5,
-          name: 'Bóle miesiączkowe'
-        },
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból gardła' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Ból mięśni' },
+          { id: 5, name: 'Bóle miesiączkowe' }
       ]
-    },
-    {
+  },
+  {
       id: 2,
       name: 'Rutinoscorbin',
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      desc: "Rutinoscorbin wspomaga odporność i zapobiega przeziębieniom. Zawiera witaminę C oraz rutynę, które wzmacniają naczynia krwionośne.",
       availability: true,
       price: 23.99,
       img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/rutino.png?alt=media&token=44911335-5fba-4972-82d4-36730d33e0d2',
       amount: 90,
       symptoms: [
-        {
-          id: 1,
-          name: 'Ból głowy'
-        },
-        {
-          id: 2,
-          name: 'Ból gardła'
-        },
-        {
-          id: 3,
-          name: 'Gorączka'
-        },
-        {
-          id: 4,
-          name: 'Ból mięśni'
-        },
-        {
-          id: 5,
-          name: 'Bóle miesiączkowe'
-        },
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból gardła' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Ból mięśni' },
+          { id: 5, name: 'Bóle miesiączkowe' }
       ]
-    },
-    {
+  },
+  {
       id: 3,
       name: 'Etopiryna',
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      desc: "Etopiryna to lek przeciwbólowy, stosowany w leczeniu bólu głowy, bólu gardła, gorączki oraz bólu mięśni. Zawiera kwas acetylosalicylowy.",
       availability: false,
       price: 19.99,
       img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/eopiryna.jpg?alt=media&token=c2515a5f-2424-4b9d-9bda-49fcd13b9cea',
       amount: 30,
       symptoms: [
-        {
-          id: 1,
-          name: 'Ból głowy'
-        },
-        {
-          id: 2,
-          name: 'Ból gardła'
-        },
-        {
-          id: 3,
-          name: 'Gorączka'
-        },
-        {
-          id: 4,
-          name: 'Ból mięśni'
-        },
-        {
-          id: 5,
-          name: 'Bóle miesiączkowe'
-        },
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból gardła' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Ból mięśni' },
+          { id: 5, name: 'Bóle miesiączkowe' }
       ]
-    },
-    {
+  },
+  {
       id: 4,
       name: 'Ibum Express Forte',
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      desc: "Ibum Express Forte to silny lek przeciwbólowy i przeciwgorączkowy, zawierający ibuprofen. Skutecznie łagodzi bóle głowy, mięśni, gardła oraz bóle menstruacyjne.",
       availability: true,
       price: 49.99,
-      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/apap_noc.jpg?alt=media&token=ef8bd1a5-8003-4783-b0e9-dcaf27a6b6ba',
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/45058ab1-scale-350x350.jpg?alt=media&token=34bf92a9-985e-4e14-891a-d147e583a867',
       amount: 30,
       symptoms: [
-        {
-          id: 1,
-          name: 'Ból głowy'
-        },
-        {
-          id: 2,
-          name: 'Ból gardła'
-        },
-        {
-          id: 3,
-          name: 'Gorączka'
-        },
-        {
-          id: 4,
-          name: 'Ból mięśni'
-        },
-        {
-          id: 5,
-          name: 'Bóle miesiączkowe'
-        },
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból gardła' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Ból mięśni' },
+          { id: 5, name: 'Bóle miesiączkowe' }
       ]
-    }
-  ]
+  },
+  {
+      id: 5,
+      name: 'Nurofen',
+      desc: "Nurofen to popularny lek przeciwbólowy i przeciwgorączkowy, zawierający ibuprofen. Skutecznie łagodzi ból głowy, mięśni, gorączkę oraz bóle menstruacyjne.",
+      availability: true,
+      price: 14.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/29799ca8-scale-350x350.jpg?alt=media&token=ed44ee3f-2715-4629-94d0-1bf2cc6e3c6f',
+      amount: 50,
+      symptoms: [
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból mięśni' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Bóle menstruacyjne' }
+      ]
+  },
+  {
+      id: 6,
+      name: 'Polopiryna S',
+      desc: "Polopiryna S to lek przeciwbólowy i przeciwgorączkowy, zawierający kwas acetylosalicylowy. Pomaga w łagodzeniu bólu głowy, gorączki oraz bólu mięśni.",
+      availability: true,
+      price: 9.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/19bce8a5-scale-350x350.jpg?alt=media&token=7edb5505-e055-4304-9900-37b93c8184b6',
+      amount: 30,
+      symptoms: [
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Gorączka' },
+          { id: 3, name: 'Ból mięśni' }
+      ]
+  },
+  {
+      id: 7,
+      name: 'Xenna Extra',
+      desc: "Xenna Extra to lek przeczyszczający, stosowany w leczeniu zaparć. Zawiera sennozydy, które pobudzają perystaltykę jelit.",
+      availability: true,
+      price: 7.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/6a297938-scale-350x350.jpg?alt=media&token=76f98a3c-c247-4889-ac6b-a7b1f40266d3',
+      amount: 20,
+      symptoms: [
+          { id: 1, name: 'Zaparcia' }
+      ]
+  },
+  {
+      id: 8,
+      name: 'Voltaren Emulgel',
+      desc: "Voltaren Emulgel to żel przeciwbólowy i przeciwzapalny, zawierający diklofenak. Stosowany miejscowo w leczeniu bólu mięśni i stawów.",
+      availability: true,
+      price: 29.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/5ebe24c9-scale-350x350.jpg?alt=media&token=bd314557-4c8a-4ae2-a343-a2ee6ebb78fe',
+      amount: 100,
+      symptoms: [
+          { id: 1, name: 'Ból mięśni' },
+          { id: 2, name: 'Ból stawów' }
+      ]
+  },
+  {
+      id: 9,
+      name: 'Nospa',
+      desc: "Nospa to lek rozkurczowy, zawierający drotawerynę. Stosowany w leczeniu bólu brzucha spowodowanego skurczami mięśni gładkich.",
+      availability: true,
+      price: 15.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/629c2ab3-scale-350x350.jpg?alt=media&token=34bbc864-be21-4fd5-b303-e3f9cc5c5e56',
+      amount: 60,
+      symptoms: [
+          { id: 1, name: 'Ból brzucha' },
+          { id: 2, name: 'Skurcze mięśni' }
+      ]
+  },
+  {
+      id: 10,
+      name: 'Panadol',
+      desc: "Panadol to lek przeciwbólowy i przeciwgorączkowy, zawierający paracetamol. Skutecznie łagodzi ból głowy, ból mięśni, gorączkę oraz bóle menstruacyjne.",
+      availability: true,
+      price: 12.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/5f22223a-scale-600x600.jpg?alt=media&token=bd74b94d-5dad-4f02-8e12-aea35b13a23a',
+      amount: 30,
+      symptoms: [
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból mięśni' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Bóle menstruacyjne' }
+      ]
+  },
+  {
+      id: 11,
+      name: 'Gripex',
+      desc: "Gripex to lek na przeziębienie i grypę, zawierający paracetamol, fenylefrynę oraz dekstrometorfan. Łagodzi objawy przeziębienia, takie jak ból gardła, kaszel i gorączka.",
+      availability: true,
+      price: 19.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/e079e47d-scale-350x350.jpg?alt=media&token=c5f4b385-9b69-400b-a651-770626e3694a',
+      amount: 20,
+      symptoms: [
+          { id: 1, name: 'Ból gardła' },
+          { id: 2, name: 'Kaszel' },
+          { id: 3, name: 'Gorączka' }
+      ]
+  },
+  {
+      id: 12,
+      name: 'Strepsils',
+      desc: "Strepsils to pastylki do ssania na ból gardła, zawierające amylometakrezol oraz alkohol dichlorobenzylowy. Skutecznie łagodzą ból i stan zapalny gardła.",
+      availability: true,
+      price: 14.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/29386b71-scale-350x350.jpg?alt=media&token=cd00f760-6734-4793-8634-d7aea8670349',
+      amount: 24,
+      symptoms: [
+          { id: 1, name: 'Ból gardła' }
+      ]
+  },
+  {
+      id: 13,
+      name: 'Ibuprom',
+      desc: "Ibuprom to lek przeciwbólowy i przeciwgorączkowy, zawierający ibuprofen. Skutecznie łagodzi ból głowy, mięśni, gorączkę oraz bóle menstruacyjne.",
+      availability: true,
+      price: 11.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/ibuprom-200-mg-10-tabletek.2.jpg?alt=media&token=3f8a3a0a-0d24-4a5e-aa75-9e008526af6b',
+      amount: 40,
+      symptoms: [
+          { id: 1, name: 'Ból głowy' },
+          { id: 2, name: 'Ból mięśni' },
+          { id: 3, name: 'Gorączka' },
+          { id: 4, name: 'Bóle menstruacyjne' }
+      ]
+  },
+  {
+      id: 14,
+      name: 'Theraflu',
+      desc: "Theraflu to lek na przeziębienie i grypę, zawierający paracetamol, fenylefrynę oraz chlorfenaminę. Łagodzi objawy przeziębienia, takie jak ból gardła, katar i gorączka.",
+      availability: true,
+      price: 22.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/7989e713-scale-350x350.jpg?alt=media&token=592cfb76-8130-4635-9692-47037c4e8c82',
+      amount: 10,
+      symptoms: [
+          { id: 1, name: 'Ból gardła' },
+          { id: 2, name: 'Katar' },
+          { id: 3, name: 'Gorączka' }
+      ]
+  },
+  {
+      id: 15,
+      name: 'Neo-Angin',
+      desc: "Neo-Angin to pastylki do ssania na ból gardła, zawierające amylometakrezol oraz alkohol dichlorobenzylowy. Skutecznie łagodzą ból i stan zapalny gardła.",
+      availability: true,
+      price: 13.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/p-neo-angin-tabletki-do-ssania-24-szt.webp?alt=media&token=81fb9885-55b6-4faf-aebd-2cad35de3436',
+      amount: 24,
+      symptoms: [
+          { id: 1, name: 'Ból gardła' }
+      ]
+  },
+  {
+      id: 16,
+      name: 'Solpadeine',
+      desc: "Solpadeine to lek przeciwbólowy, zawierający paracetamol, kofeinę oraz kodeinę. Skutecznie łagodzi silny ból głowy, mięśni oraz ból menstruacyjny.",
+      availability: true,
+      price: 29.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/29ce86a2-scale-350x350.jpg?alt=media&token=2a421115-b49f-465e-be15-914bdb90be87',
+      amount: 30,
+      symptoms: [
+          { id: 1, name: 'Silny ból głowy' },
+          { id: 2, name: 'Ból mięśni' },
+          { id: 3, name: 'Ból menstruacyjny' }
+      ]
+  },
+  {
+      id: 17,
+      name: 'Ulgix Wzdęcia',
+      desc: "Ulgix Wzdęcia to lek na wzdęcia i nadmierne gazy, zawierający symetykon. Pomaga w redukcji wzdęć i poprawia komfort trawienia.",
+      availability: true,
+      price: 8.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/f2983c59-scale-350x350.jpg?alt=media&token=02396029-2325-4d5f-88c8-21d51e95f237',
+      amount: 40,
+      symptoms: [
+          { id: 1, name: 'Wzdęcia' },
+          { id: 2, name: 'Nadmierne gazy' }
+      ]
+  },
+  {
+      id: 18,
+      name: 'Gastrosil',
+      desc: "Gastrosil to lek na zgagę i niestrawność, zawierający ranitydynę. Skutecznie łagodzi objawy zgagi i niestrawności.",
+      availability: true,
+      price: 18.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/bcd4964b-scale-350x350.jpg?alt=media&token=a531513c-463b-4a70-a8c1-fd50faf538d8',
+      amount: 20,
+      symptoms: [
+          { id: 1, name: 'Zgaga' },
+          { id: 2, name: 'Niestrawność' }
+      ]
+  },
+  {
+      id: 19,
+      name: 'Allertec',
+      desc: "Allertec to lek przeciwhistaminowy, zawierający cetyryzynę. Skutecznie łagodzi objawy alergii, takie jak katar sienny, swędzenie i łzawienie oczu.",
+      availability: true,
+      price: 15.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/a5e642c3-scale-350x350.jpg?alt=media&token=4d72db6e-5fc3-43b4-a515-3ef5ef3089f1',
+      amount: 30,
+      symptoms: [
+          { id: 1, name: 'Alergia' },
+          { id: 2, name: 'Katar sienny' },
+          { id: 3, name: 'Swędzenie oczu' },
+          { id: 4, name: 'Łzawienie oczu' }
+      ]
+  },
+  {
+      id: 20,
+      name: 'Heviran',
+      desc: "Heviran to lek przeciwwirusowy, zawierający acyklowir. Stosowany w leczeniu opryszczki wargowej oraz innych zakażeń wywołanych wirusem opryszczki.",
+      availability: true,
+      price: 24.99,
+      img: 'https://firebasestorage.googleapis.com/v0/b/medsheep-4763c.appspot.com/o/8411a722-scale-350x350.jpg?alt=media&token=0ae5c795-6222-4271-96d8-ec7113a73002',
+      amount: 30,
+      symptoms: [
+          { id: 1, name: 'Opryszczka wargowa' },
+          { id: 2, name: 'Zakażenie wirusem opryszczki' }
+      ]
+  }
+];
